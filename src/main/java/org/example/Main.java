d0454e38462d507a9bf8ad5ee04b5377c17a0ed7
mimtokox <165220686+MimiToKOX@users.noreply.github.com>
2024-04-03 20:38:01 +0200

    Dodanie Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,24 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.example.DataBase;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        DataBase dataBase = new DataBase();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Wprowadź nazwę użytkownika:");
+        String username = scanner.nextLine();
+
+        System.out.println("Wprowadź hasło:");
+        String password = scanner.nextLine();
+
+        if (dataBase.checkLogin(username, password)) {
+            System.out.println("\nPoprawnie zalogowano.");
+        } else {
+            System.out.println("\nBłąd logowania: nieprawidłowa nazwa użytkownika lub hasło.");
         }
     }
 }
